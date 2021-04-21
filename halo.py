6fb4a43f179842c9d380f9ebb6f51280192b2803
Hendriko-UGM <82925876+Hendriko-UGM@users.noreply.github.com>
2021-04-21 18:29:09 +0700

    Add files via upload

---
+++ halo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#/----------------------------------------/
+#/            program halo.py             /
+#/ by: hendrikoyudhatama@mail.ugm.ac.id   /
+#/----------------------------------------/
+
+
+# In[ ]:
+
+
+
+
